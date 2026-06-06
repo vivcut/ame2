898227557fcaec2e1dd258e9f3ae718e741fe6b2
Vivaan Kumar <vivaan@Vivaans-MacBook-Pro.local>
2026-06-07 00:33:35 +0800

    small fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextconfig = {
+  experimental: {
+    // This tells the bundler to completely ignore the public folder during function tracing
+    outputFileTracingExcludes: {
+      '*': ['public/**/*'],
+    },
+  },
 };
 
-export default nextConfig;
+module.exports = nextconfig;
